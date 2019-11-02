9bfffbdb5868f981a075797ba9da2d1247d89b91
Pol Monroig <polmonroig@yahoo.es>
2019-11-02 08:18:24 +0100

    Improve LZ78

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,17 @@ public class Main {
 
     static public void main(String[] args){
         LZ78 compresor = new LZ78();
-        byte[] s = CtrlPersistencia.ReadFileAsBytes("/home/pol/Documents/fib/PROP/compressor/src/test_files/original/hola.txt");
+        String fileName = "texto_largo.txt";
+        byte[] s = CtrlPersistencia.ReadFileAsBytes("/home/pol/Documents/fib/PROP/compressor/src/test_files/original/" + fileName);
         byte[] code = compresor.comprimir(s);
         System.out.println("compressed done.");
-        CtrlPersistencia.WriteBytesToFile("/home/pol/Documents/fib/PROP/compressor/src/test_files/compressed/hola.txt", code);
-        byte[]s2  = CtrlPersistencia.ReadFileAsBytes("/home/pol/Documents/fib/PROP/compressor/src/test_files/compressed/hola.txt");
+        CtrlPersistencia.WriteBytesToFile("/home/pol/Documents/fib/PROP/compressor/src/test_files/compressed/" + fileName, code);
+        byte[]s2  = CtrlPersistencia.ReadFileAsBytes("/home/pol/Documents/fib/PROP/compressor/src/test_files/compressed/" + fileName);
         byte[] code2 = compresor.descomprimir(s2);
         String c = new String(code2);
 
         System.out.println("decompressed done.");
-        CtrlPersistencia.WriteBytesToFile("/home/pol/Documents/fib/PROP/compressor/src/test_files/decompressed/hola.txt", code2);
+        CtrlPersistencia.WriteBytesToFile("/home/pol/Documents/fib/PROP/compressor/src/test_files/decompressed/" + fileName, code2);
         String c2 = new String(s);
         if(c.equals(c2)){
             System.out.println("OUT == ORIGINAL");
--- src/domain/LZ78.java
+++ src/domain/LZ78.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.lang.Math;
 
@@ -39,13 +40,13 @@ public class LZ78 extends Algoritme {
      */
     @Override
     public byte[] comprimir(byte[] texto) {
-        String text = new String(texto);
-        SortedMap< String, Integer> dict = new TreeMap<>(); ;
+        String text = new String(texto, StandardCharsets.UTF_8);
+        SortedMap< String, Integer> dict = new TreeMap<>();
         String current = "";
         Integer lastWordPos = 0;
-        String coding = "";
+        StringBuilder coding = new StringBuilder();
         boolean inDict = false;
-        char phrase = ' ';
+        char phrase;
         for(int i = 0; i < text.length(); ++i){
             phrase = text.charAt(i);
             current += phrase;
@@ -55,14 +56,14 @@ public class LZ78 extends Algoritme {
                 inDict = true;
             }
             else{ // else new phrase
-                coding += Integer.toString(lastWordPos)+ ","  + phrase ;
+                coding.append(lastWordPos).append(",").append(phrase);
                 dict.put(current, dict.size() + 1);
                 lastWordPos = 0;
                 current = "";
             }
         }
-        if(inDict)coding += Integer.toString(lastWordPos);
-        String binary_string = toBinaryString(coding);
+        if(inDict) coding.append(lastWordPos);
+        String binary_string = toBinaryString(coding.toString());
 
         this.original_size = text.length();
         this.compressed_size = (int)Math.ceil(binary_string.length() / 8.0);
@@ -73,7 +74,7 @@ public class LZ78 extends Algoritme {
     @Override
     public byte[] descomprimir(byte[] texto) {
         String text = toString(texto);
-        String coding = "";
+        StringBuilder coding = new StringBuilder();
         double log_2 = Math.log(2.0);
         int current_index = 0;
         ArrayList<String> phrases = new ArrayList<>();
@@ -85,14 +86,14 @@ public class LZ78 extends Algoritme {
                     current_index_size += 1;
                 }
             }
-            String index_string = Character.toString(text.charAt(i));
+            StringBuilder index_string = new StringBuilder(Character.toString(text.charAt(i)));
             i++;
             while(index_string.length() < current_index_size){
 
-                index_string += Character.toString(text.charAt(i));
+                index_string.append(text.charAt(i));
                 ++i;
             }
-            int index = Integer.parseInt(index_string, 2);
+            int index = Integer.parseInt(index_string.toString(), 2);
             String word = "";
             if(i != text.length()){
                 word = LZ78.getLetter(text, i);
@@ -105,11 +106,11 @@ public class LZ78 extends Algoritme {
                 word = phrases.get(index - 1) + word;
                 phrases.add(word);
             }
-            coding += word;
+            coding.append(word);
             current_index++;
         }
 
-        return coding.getBytes();
+        return coding.toString().getBytes();
     }
 
     private static byte[] toByteArray(String binary_string){
@@ -127,11 +128,11 @@ public class LZ78 extends Algoritme {
 
     public static String intToString(int b, int binary_size){
         String s = Integer.toBinaryString(b);
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for(int i = s.length() - 1; i >= s.length() - binary_size && i >= 0; --i){
-            result = s.charAt(i) + result;
+            result.insert(0, s.charAt(i));
         }
-        return result;
+        return result.toString();
     }
 
     /**
@@ -140,15 +141,15 @@ public class LZ78 extends Algoritme {
      * @return the encoding as a binary text
      */
     private String toBinaryString(String coding){
-        String binary_string = "";
+        StringBuilder binary_string = new StringBuilder();
         int current_index_size = 1;
         double log_2 = Math.log(2);
         int current_index = 0;
 
         for(int i = 0; i < coding.length(); i++){
-            String index = "";
+            StringBuilder index = new StringBuilder();
             while(i < coding.length() && coding.charAt(i) != ','){
-                index += coding.charAt(i);
+                index.append(coding.charAt(i));
                 ++i;
             }
             ++i;
@@ -162,14 +163,14 @@ public class LZ78 extends Algoritme {
             if(current_index_size > 8){
                 binary_size = current_index_size;
             }
-            binary_string += addZeros(intToString(Integer.parseInt(index), binary_size), current_index_size); // PROBLEMA <<<<<
+            binary_string.append(addZeros(intToString(Integer.parseInt(index.toString()), binary_size), current_index_size)); // PROBLEMA <<<<<
             if(i  < coding.length()){
                 char value = coding.charAt(i);
-                binary_string += addZeros(intToString(value, 8), 8);
+                binary_string.append(addZeros(intToString(value, 8), 8));
             }
             current_index++;
         }
-        return binary_string;
+        return binary_string.toString();
     }
 
     /**
@@ -179,19 +180,21 @@ public class LZ78 extends Algoritme {
      * @return returns a binary string where its size >= n_zeros
      */
     static public String addZeros(String binary, int n_zeros){ // CORRECTA
-        while(binary.length() < n_zeros){
-            binary = "0" + binary;
+        StringBuilder binaryBuilder = new StringBuilder(binary);
+        while(binaryBuilder.length() < n_zeros){
+            binaryBuilder.insert(0, "0");
         }
+        binary = binaryBuilder.toString();
         return binary;
     }
 
 
     static public int getIntFromString(String text, int i){
-        String letter = "";
+        StringBuilder letter = new StringBuilder();
         for(int j = i; j < i + 8 && j < text.length(); ++j){
-            letter += Character.toString(text.charAt(j));
+            letter.append(text.charAt(j));
         }
-        return Integer.parseInt(letter, 2);
+        return Integer.parseInt(letter.toString(), 2);
     }
 
 
@@ -200,16 +203,16 @@ public class LZ78 extends Algoritme {
     }
 
     static  private String toString(byte[] byte_coding){ // CORRECTA
-        String binary_string = "";
+        StringBuilder binary_string = new StringBuilder();
         int zeros_offset = byte_coding[0];
         for(int i = 1; i < byte_coding.length - 1; ++i){
-            binary_string += addZeros(intToString(byte_coding[i], 8), 8);
+            binary_string.append(addZeros(intToString(byte_coding[i], 8), 8));
         }
         if(zeros_offset == 0){
             zeros_offset = 8;
         }
-        binary_string += addZeros(intToString(byte_coding[byte_coding.length - 1], 8), zeros_offset) ;
-        return binary_string;
+        binary_string.append(addZeros(intToString(byte_coding[byte_coding.length - 1], 8), zeros_offset));
+        return binary_string.toString();
     }
 
 
