da6a57cf54aef146ec994e3f8ceffde2db115c54
DavidSantosss <55484949+DavidSantosss@users.noreply.github.com>
2019-11-10 20:41:01 +0100

    Update CtrlDomain.java

--- src/domain/controllers/CtrlDomain.java
+++ src/domain/controllers/CtrlDomain.java
@@ -1,12 +1,12 @@
 package domain.controllers;
 
-import domain.LZ78;
-
+import domain.*;
 import persistencia.controllers.CtrlPersistencia;
 
 public class CtrlDomain {
     private CtrlPersistencia controladorPersistencia;
     private LZ78 lz78;
+    private LZSS lzss;
 
 
     public CtrlDomain(){
@@ -16,7 +16,7 @@ public class CtrlDomain {
     private void init() {
         controladorPersistencia = new CtrlPersistencia();
         lz78 = new LZ78();
-
+        lzss = new LZSS();
     }
 
     public void comprimir(String algoritmo, String archivo){
@@ -25,7 +25,11 @@ public class CtrlDomain {
             byte[] compressed = lz78.comprimir(file);
             CtrlPersistencia.WriteBytesToFile(archivo + ".lz78", compressed);
         }
-
+        else if(algoritmo == "lzss"){
+            byte[] file = CtrlPersistencia.ReadFileAsBytes(archivo);
+            byte[] compressed = lzss.comprimir(file);
+            CtrlPersistencia.WriteBytesToFile(archivo + ".lzss", compressed);
+        }
     }
 
     public void descomprimir(String algoritmo, String archivo){
@@ -33,6 +37,11 @@ public class CtrlDomain {
             byte[] file = CtrlPersistencia.ReadFileAsBytes(archivo);
             byte[] decompressed = lz78.descomprimir(file);
             CtrlPersistencia.WriteBytesToFile(archivo , decompressed);
+        }
+        else if(algoritmo == "lzss"){
+            byte[] file = CtrlPersistencia.ReadFileAsBytes(archivo);
+            byte[] compressed = lzss.comprimir(file);
+            CtrlPersistencia.WriteBytesToFile(archivo + ".lzss", compressed);
         }
     }
 
