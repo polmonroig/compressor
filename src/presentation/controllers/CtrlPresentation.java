1c28bdc19580f7bd30fb0927481ee16a01c2fa2e
DavidSantosss <55484949+DavidSantosss@users.noreply.github.com>
2019-11-14 15:08:39 +0100

    Update CtrlPresentation.java

--- src/presentation/controllers/CtrlPresentation.java
+++ src/presentation/controllers/CtrlPresentation.java
@@ -22,68 +22,38 @@ public class CtrlPresentation {
             switch (userInput.toLowerCase().trim()) {
                 case "1":
                     file = getUserInput(Input, "Introduce el archivo que deseas comprimir");
-                    System.out.println("Que desea comprimir?");
-                    userInput = getUserInput(Input, "1-texto/2-imagen");
-                    switch (userInput.toLowerCase().trim()){
-                        case "1":
-                            userInput = getUserInput(Input, "Que algoritmo desea utilizar?(1-LZ78, 2-LZW, 3-LZSS, 4-auto)");
-                            switch (userInput.toLowerCase().trim()){
-                                case "1":
-                                    ctrlDomain.comprimir("lz78", file);
-                                    break;
-                                case "2":
-                                    ctrlDomain.comprimir("lzw", file);;
-                                    break;
-                                case "3":
-                                    ctrlDomain.comprimir("lzss", file);;
-                                    break;
-                                case "4":
-                                    System.out.println("Lo siento, esta funcion aun no esta implementada");
-                                    break;
-                                default:
-                                    System.out.println("La opcion introducida no es valida, por favor intentalo de nuevo :(");
-                                    break;
-                            }
-                            break;
-                        case "2":
-                            String calidad = getUserInput(Input, "Con que grado de calidad desea comprimir? (Siendo 0 el más bajo y 12 el más alto)");
-                            ctrlDomain.setCalidad(Integer.parseInt(calidad));
-                            ctrlDomain.comprimir("jpeg", file);
-                            break;
-                        default:
-                            System.out.println("La opcion introducida no es valida, por favor intentalo de nuevo :(");
-                            break;
-                    }
+                    if (file.endsWith(".txt")) {
+                        userInput = getUserInput(Input, "Que algoritmo desea utilizar?(1-LZ78, 2-LZW, 3-LZSS, 4-auto)");
+                        switch (userInput.toLowerCase().trim()) {
+                            case "1":
+                                ctrlDomain.comprimir("lz78", file);
+                                break;
+                            case "2":
+                                ctrlDomain.comprimir("lzw", file);
+                                break;
+                            case "3":
+                                ctrlDomain.comprimir("lzss", file);
+                                break;
+                            case "4":
+                                System.out.println("Lo siento, esta funcion aun no esta implementada");
+                                break;
+                            default:
+                                System.out.println("La opcion introducida no es valida, por favor intentalo de nuevo :(");
+                                break;
+                        }
+                    } else if (file.endsWith(".ppm")) {
+                        String calidad = getUserInput(Input, "Con que grado de calidad desea comprimir? (Siendo 0 el más bajo y 12 el más alto)");
+                        ctrlDomain.setCalidad(Integer.parseInt(calidad));
+                        ctrlDomain.comprimir("jpeg", file);
+                    } else System.out.println("La opcion introducida no es valida, por favor intentalo de nuevo :(");
                     break;
                 case "2":
                     file = getUserInput(Input, "Introduce el archivo que deseas descomprimir");
-                    userInput = getUserInput(Input, "Desea descomprimir un texto o una imagen?");
-                    switch (userInput.toLowerCase().trim()){
-                        case "texto":
-                            userInput = getUserInput(Input, "Que algoritmo desea utilizar?(1-LZ78, 2-LZW, 3-LZSS)");
-                            switch (userInput.toLowerCase().trim()){
-                                case "1":
-                                    ctrlDomain.descomprimir("lz78", file);
-                                    break;
-                                case "2":
-                                    ctrlDomain.descomprimir("lzw", file);;
-                                    break;
-                                case "3":
-                                    ctrlDomain.descomprimir("lzss", file);;
-                                    break;
-                                default:
-                                    System.out.println("La opcion introducida no es valida, por favor intentalo de nuevo :(");
-                                    break;
-                            }
-                            break;
-                        case "imagen":
-                            System.out.println("Lo siento, esta funcion aun no esta implementada");
-                            break;
-                        default:
-                            System.out.println("La opcion introducida no es valida, por favor intentalo de nuevo :(");
-                            break;
-                    }
-                    break;
+                    if (file.endsWith("lz78")) ctrlDomain.descomprimir("lz78", file);
+                    else if (file.endsWith("lzss")) ctrlDomain.descomprimir("lzss", file);
+                    else if (file.endsWith("lzw")) ctrlDomain.descomprimir("lzw", file);
+                    else if (file.endsWith("jpeg")) ctrlDomain.descomprimir("jpeg", file);
+                    else System.out.println("La opcion introducida no es valida, por favor intentalo de nuevo :(");
                 case "3":
                     input = true;
                     break;
