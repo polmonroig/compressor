752641f02a1d2d74a102a5c99852f148784aea9d
jbernaus <55484837+jbernaus@users.noreply.github.com>
2019-11-17 19:34:53 +0100

    Add files via upload

--- FONTS/domain/LZW.java
+++ FONTS/domain/LZW.java
@@ -6,7 +6,7 @@ public class LZW extends Algorithm {
     @Override
     public byte[] compress(byte[] binaryFile) {
         long startTime = System.nanoTime(); // empezar contador de tiempo
-        SortedMap< String, Integer> dict = new TreeMap<>(); //map per guardar el diccionari
+        Map<String, Integer> dict = new HashMap<String, Integer>(); //map per guardar el diccionari
         String charChain = new String(binaryFile); // convertir l'array de bytes en un String
         StringBuilder s = new StringBuilder();
 
@@ -33,6 +33,7 @@ public class LZW extends Algorithm {
                     dict.put(wk,dictSize);
                     dictSize++;
                 }
+
                 w = k;
             }
         }
@@ -60,7 +61,7 @@ public class LZW extends Algorithm {
     /** Convert 8 bit to 12 bit */
     private String to12bit(int i) {
         StringBuilder temp = new StringBuilder(Integer.toBinaryString(i));
-        while (temp.length() < 12) {
+        while (temp.length() < 14) {
             temp.insert(0, "0");
         }
         return temp.toString();
@@ -87,12 +88,13 @@ public class LZW extends Algorithm {
             List<Integer> l = new ArrayList<Integer>();
 
 
-            for (int i = 0; i < s.length()-4; i = i + 12) {
-                String e = "" + s.charAt(i) + s.charAt(i+1) + s.charAt(i+2) + s.charAt(i+3) + s.charAt(i+4) + s.charAt(i+5) + s.charAt(i+6) + s.charAt(i+7) + s.charAt(i+8) + s.charAt(i+9) + s.charAt(i+10) + s.charAt(i+11) ;l.add(Integer.parseInt(e, 2));
+            for (int i = 0; i < s.length()-2; i = i + 14) {
+                String e = "" + s.charAt(i) + s.charAt(i+1) + s.charAt(i+2) + s.charAt(i+3) + s.charAt(i+4) + s.charAt(i+5) + s.charAt(i+6) + s.charAt(i+7) + s.charAt(i+8) + s.charAt(i+9) + s.charAt(i+10) + s.charAt(i+11) + s.charAt(i+12) + s.charAt(i+13);
+                l.add(Integer.parseInt(e, 2));
 
             }
 
-            SortedMap< Integer, String> dict = new TreeMap<>();
+            Map<Integer,String> dict = new HashMap<Integer,String>();
 
             StringBuilder res = new StringBuilder();
 
