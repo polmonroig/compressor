3345cb0cd0408838f6153e5fd6195d6815d489e5
jbernaus <55484837+jbernaus@users.noreply.github.com>
2019-11-17 20:26:05 +0100

    update DriverDataCtrl

--- FONTS/data/drivers/DriverDataCtrl.java
+++ FONTS/data/drivers/DriverDataCtrl.java
@@ -1,10 +1,13 @@
 package data.drivers;
 
+import data.controllers.DataCtrl;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 class DriverDataCtrl{
     public static void main(String[] args) {
         Scanner Input = new Scanner(System.in);
-
-        DataCtrl dataCtrl = new DataCtrl();
 
         boolean input = false;
 
@@ -17,24 +20,23 @@ class DriverDataCtrl{
             switch (userInput) {
                 case "1":
                     System.out.print("Dirección del archivo a leer: ");
-                    String userInput = Input.nextLine();
-                    byte[] res = dataCtrl.ReadFileAsBytes(userInput);
-                    System.out.println(res);
+                    userInput = Input.nextLine();
+                    System.out.println(Arrays.toString(DataCtrl.ReadFileAsBytes(userInput)));
                     break;
                 case "2":
                     System.out.print("Texto a guardar: ");
-                    String userInput = Input.nextLine();
+                    userInput = Input.nextLine();
                     byte[] texto = userInput.getBytes();
                     System.out.print("Dirección donde guardarlo: ");
-                    String userInput = Input.nextLine();
-                    dataCtrl.WriteBytesToFile(userInput, texto );
+                    userInput = Input.nextLine();
+                    DataCtrl.WriteBytesToFile(userInput, texto );
 
                     break;
                 case "3":
                     input = true;
                     break;
                 default:
-                    System.out.println("No ha selecionado ningua de las opciones validas. \n Intentelo de nuevo.");
+                    System.out.println("No ha selecionado ningua de las opciones validas. \nIntentelo de nuevo.");
                     System.out.println("Opciones:");
                     System.out.println("1.-Lleer archivo como bytes 2.- Escrivir archivo como bytes 3.-Salir");
                     break;
