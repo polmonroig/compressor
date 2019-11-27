c25a826086b66fc46234aa5da5de9e6a086d2aad
Pol Monroig <polmonroig@yahoo.es>
2019-11-27 18:25:36 +0100

    Add single view gui

--- FONTS/presentation/MainView.java
+++ FONTS/presentation/MainView.java
@@ -4,42 +4,96 @@ package presentation;
 import presentation.controllers.PresentationCtrl;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 
 
 public class MainView extends JFrame {
     private JPanel mainPanel;
+    private JButton comprimirArchivoDeTextoButton;
+    private JButton comprimirImagenButton;
+    private JButton comprimirCarpetaButton;
     private JButton exitButton;
-    private JButton comprimirButton;
-    private JButton descomprimirButton;
-    private JButton estadisticasButton;
+    private JButton mostrarEstadisticasGlobalesButton;
+    private JButton welcomeButton;
+    private JButton decompressButton;
+    private JButton infoButton;
+    private JPanel contentPanel;
+    private JPanel buttonsPanel;
+    private JPanel welcomePanel;
+    private JTextArea aplicacionParaComprimirYTextArea;
     private CompressionView compressionView;
     private DescompressionView descompressionView;
     private PresentationCtrl presentationCtrl;
+    private int activeButton;
+    private Color deactivatedColor;
+    private Color activeColor;
 
 
     private void initComponents(){
+        activeButton = 0;
         ImageIcon img = new ImageIcon("/home/adrian/Escritorio/compressor/DOCS/compresion.png");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(375, 150);
-        setResizable(false);
+        setSize(1000, 500);
+        //setResizable(false);
+        activeColor = new Color(79, 131, 249);
+        deactivatedColor = new Color(53, 71, 120);
+
         setContentPane(mainPanel);
         this.setIconImage(img.getImage());
 
-        estadisticasButton.setEnabled(false);
+
+        //stadisticasButton.setEnabled(false);
 //        compressButton.setEnabled(false);
 //        decompressButton.setEnabled(false);
-        setVisible(true);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+
         compressionView = new CompressionView(getTitle(), this);
         descompressionView = new DescompressionView(getTitle(), this);
 
+
+
+        setVisible(true);
     }
+
+
+    private void addHoverEffect(JButton button, int index){
+        button.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                if(button.isEnabled())button.setBackground(activeColor);
+            }
+
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                if(button.isEnabled() && activeButton != index) button.setBackground(deactivatedColor);
+            }
+        });
+    }
+
+    private void updateButtonsListeners(){
+        addHoverEffect(welcomeButton, 0);
+        addHoverEffect(comprimirArchivoDeTextoButton, 1);
+        addHoverEffect(comprimirImagenButton, 2);
+        addHoverEffect(comprimirCarpetaButton, 3);
+        addHoverEffect(decompressButton, 4);
+        addHoverEffect(infoButton, 5);
+        addHoverEffect(mostrarEstadisticasGlobalesButton, 6);
+    }
+
 
     private void initEventListeners(){
 
+        updateButtonsListeners();
 
-        comprimirButton.addActionListener(actionEvent -> {
+
+
+
+
+
+       /* comprimirButton.addActionListener(actionEvent -> {
             setVisible(false);
             compressionView.init();
 
@@ -53,7 +107,7 @@ public class MainView extends JFrame {
 
 
             //abrir nueva ventana y cerrar esta
-        });
+        });*/
 
 
 
