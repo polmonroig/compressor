36f94ae8ad0a048cd3c2eaeee7d54fc6261fb34d
Adry-byte <adrianalvarez0000@gmail.com>
2019-12-09 19:16:26 +0100

    help View sin acabar

--- FONTS/presentation/HelpContent.java
+++ FONTS/presentation/HelpContent.java
@@ -1,4 +1,24 @@
 package presentation;
 
-public class HelpContent {
+import javax.swing.*;
+import java.awt.*;
+
+public class HelpContent extends Content {
+    private JLabel help;
+    private JFrame helpcontent;
+
+    public HelpContent(String title, String contentDescription, int i){
+        super(title, contentDescription, i);
+        help = new JLabel();
+        helpcontent = new JFrame();
+    }
+
+    @Override
+    public void init(){
+        layout = new GridLayout(5, 1);
+        setLayout(layout);
+        add(mainText);
+        add(description);
+
+    }
 }
