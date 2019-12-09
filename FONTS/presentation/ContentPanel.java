98d6b6ac4c63fc7087f41ccd60d425c925b8f9fa
Pol Monroig <polmonroig@yahoo.es>
2019-12-09 22:13:25 +0100

    Update gui

--- FONTS/presentation/ContentPanel.java
+++ FONTS/presentation/ContentPanel.java
@@ -1,6 +1,7 @@
 package presentation;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class ContentPanel extends JPanel {
@@ -12,14 +13,16 @@ public class ContentPanel extends JPanel {
 
         selectedView = 0;
         contents = contentPanels;
+
+    }
+
+    public void init() {
+        setBackground(Color.WHITE);
         for (Content content : contents) {
             content.init();
             add(content);
         }
         contents.get(selectedView).setVisible(true);
-    }
-
-    public void init() {
     }
 
     public void selectView(int id) {
