460d19836475fcd78409b3a3c28f5b522255a2ad
Pol Monroig <polmonroig@yahoo.es>
2019-12-09 15:12:53 +0100

    Apply state pattern to Domain ctrl

--- FONTS/domain/controllers/DomainCtrl.java
+++ FONTS/domain/controllers/DomainCtrl.java
@@ -8,13 +8,9 @@ import java.io.IOException;
 
 public class DomainCtrl {
     private DataCtrl dataCtrl;
-    private LZ78 lz78;
-    private LZSS lzss;
-    private LZW lzw;
-    private JPEG jpeg;
+    private Algorithm algorithm;
     private GlobalStats globalStats;
-    private String [] Algoritmes = {"LZ78", "LZSS", "LZW", "JPEG"};
-
+    private int compressionQuality;
 
     public DomainCtrl(){
         init();
@@ -23,14 +19,8 @@ public class DomainCtrl {
     private void init() {
         dataCtrl = new DataCtrl(this);
         globalStats = new GlobalStats();
-        lz78 = new LZ78();
-        lzss = new LZSS();
-        lzw = new LZW();
-        jpeg = new JPEG();
-    }
-
-    public String[] getAlgoritmes(){
-        return Algoritmes;
+        algorithm = new LZ78();
+        compressionQuality = 0;
     }
 
     public float getMeanOriginalFileSize(){
@@ -57,139 +47,58 @@ public class DomainCtrl {
         return globalStats.getNumberFiles();
     }
 
-    public byte[] compress(String algorithm, byte[] file){
+
+    public void selectLZ78(){
+        algorithm = new LZ78();
+    }
+
+    public void selectLZW(){
+        algorithm = new LZW();
+    }
+
+    public void selectLZSS() {
+        algorithm = new LZSS();
+    }
+
+    public void selectJPEG(){
+        algorithm = new JPEG();
+        ((JPEG) algorithm).setQuality(compressionQuality);
+    }
+
+    public void setQuality(int quality){
+        compressionQuality = quality;
+    }
+
+    public byte[] compress(byte[] file){
         globalStats.setNumberFiles(globalStats.getNumberFiles() + 1);
-
-        switch (algorithm) {
-            case "lz78": {
-                return lz78.compress(file);
-            }
-            case "lzss": {
-                return lzss.compress(file);
-            }
-            case "lzw": {
-                return lzw.compress(file);
-            }
-            case "jpeg": {
-                return jpeg.compress(file);
-            }
-        }
-        return file;
+        return algorithm.compress(file);
 
     }
 
-    public byte[] decompress(String algorithm, byte[] file){
+    public byte[] decompress(byte[] file){
 
-        switch (algorithm) {
-            case "lz78": {
-                return lz78.decompress(file);
-            }
-            case "lzss": {
-                return lzss.decompress(file);
-            }
-            case "lzw": {
-                return lzw.decompress(file);
-            }
-            case "jpeg": {
-                return jpeg.decompress(file);
-            }
-        }
-        return file;
+        return algorithm.decompress(file);
     }
 
 
-    public void setQuality(int parseInt) {
-        jpeg.setQuality(parseInt);
+    public float getOriginalFileSize() {
+        return algorithm.getOriginalSize();
     }
 
-    public float getOriginalFileSize(String algorithm) {
-        switch (algorithm) {
-            case "lz78": {
-                return lz78.getOriginalSize();
-            }
-            case "lzss": {
-                return lzss.getOriginalSize();
-            }
-            case "lzw": {
-                return lzw.getOriginalSize();
-            }
-            case "jpeg": {
-                return jpeg.getOriginalSize();
-            }
-        }
-        return 0;
+    public float getCompressedSize() {
+        return algorithm.getCompressedSize();
     }
 
-    public float getMidaArxiuFinal(String algorithm) {
-        switch (algorithm) {
-            case "lz78": {
-                return lz78.getCompressedSize();
-            }
-            case "lzss": {
-                return lzss.getCompressedSize();
-            }
-            case "lzw": {
-                return lzw.getCompressedSize();
-            }
-            case "jpeg": {
-                return jpeg.getCompressedSize();
-            }
-        }
-        return 0;
+    public float getCompressionRatio() {
+        return algorithm.getCompressionRatio();
     }
 
-    public float getCompressionDegree(String algorithm) {
-        switch (algorithm) {
-            case "lz78": {
-                return lz78.getCompressionRatio();
-            }
-            case "lzss": {
-                return lzss.getCompressionRatio();
-            }
-            case "lzw": {
-                return lzw.getCompressionRatio();
-            }
-            case "jpeg": {
-                return jpeg.getCompressionRatio();
-            }
-        }
-        return 0;
+    public float getCompressionTime() {
+        return algorithm.getCompressionTime();
     }
 
-    public float getCompressionTime(String algorithm) {
-        switch (algorithm) {
-            case "lz78": {
-                return lz78.getCompressionTime();
-            }
-            case "lzss": {
-                return lzss.getCompressionTime();
-            }
-            case "lzw": {
-                return lzw.getCompressionTime();
-            }
-            case "jpeg": {
-                return jpeg.getCompressionTime();
-            }
-        }
-        return 0;
-    }
-
-    public float getCompressionSpeed(String algorithm) {
-        switch (algorithm) {
-            case "lz78": {
-                return lz78.getCompressionSpeed();
-            }
-            case "lzss": {
-                return lzss.getCompressionSpeed();
-            }
-            case "lzw": {
-                return lzw.getCompressionSpeed();
-            }
-            case "jpeg": {
-                return jpeg.getCompressionSpeed();
-            }
-        }
-        return 0;
+    public float getCompressionSpeed() {
+        return algorithm.getCompressionSpeed();
     }
 
     public void setStats(float originalFileSize, float compressedFileSize, float compressionTime, float compressionDegree, float compressionSpeed) {
