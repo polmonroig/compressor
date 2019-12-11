7ffb01c9b082f8e8a5b81f2b7e6340a8bd6ccec2
Adry-byte <adrianalvarez0000@gmail.com>
2019-12-11 16:42:27 +0100

    LocalStats with bug

--- FONTS/presentation/LocalStats.java
+++ FONTS/presentation/LocalStats.java
@@ -5,11 +5,10 @@ import java.awt.*;
 
 
 public class LocalStats extends JFrame {
-    private AboutContent contentpanel;
+    private JPanel contentpanel = new JPanel();
 
     public LocalStats(){
-        this.contentpanel = new AboutContent("Estadisticas Locales", "Estadisticas locales de la compression", 0);
-        setSize(new Dimension(700, 300));
+        setSize(new Dimension(500, 300));
     }
 
     private JLabel compressionTime;
@@ -17,18 +16,37 @@ public class LocalStats extends JFrame {
     private JLabel compressionSpeed;
     private JLabel originalFileSize;
     private JLabel compressedFileSize;
+    private JLabel mainText;
 
+    private GridLayout layout;
 
     public void setLocalStats(float compressedFileSize, float compressionDegree, float compressionSpeed, float compressionTime, float originalFileSize) {
-        this.compressedFileSize = new JLabel(Float.toString(compressedFileSize));
-        this.compressionDegree = new JLabel(Float.toString(compressionDegree));
-        this.compressionSpeed = new JLabel(Float.toString(compressionSpeed));
-        this.compressionTime = new JLabel(Float.toString(compressionTime));
-        this.originalFileSize = new JLabel(Float.toString(originalFileSize));
-        contentpanel.add(this.compressedFileSize);
-        contentpanel.add(this.compressionDegree);
-        contentpanel.add(this.compressionSpeed);
-        contentpanel.add(this.compressionTime);
-        contentpanel.add(this.originalFileSize);
+        this.compressedFileSize = new JLabel("<html><u>Tamaño del archivo comprimido:</u><html>" + " " + Float.toString(compressedFileSize) + " Unidad");
+        this.compressionDegree = new JLabel("<html><u>Grado de compression:</u><html>" + " " + Float.toString(compressionDegree) + " Unidad");
+        this.compressionSpeed = new JLabel("<html><u>Velocidad de compression:</u><html>" + " " + Float.toString(compressionSpeed) + " Unidad");
+        this.compressionTime = new JLabel("<html><u>Tiempo de compression:</u><html>" + " " + Float.toString(compressionTime) + " Unidad");
+        this.originalFileSize = new JLabel("<html><u>Tamaño del archivo:</u><html>" + " " + Float.toString(originalFileSize) + " Unidad");
+        init();
+    }
+    private void init(){
+        mainText = new JLabel("Estadisticas Locales");
+        mainText.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 28));
+
+        layout = new GridLayout(7, 1);
+        setLayout(layout);
+
+        setTitle("MasterCompressor");
+        add(mainText);
+        add(new JLabel("Visualizacion de las estadisticas de la compresion realizada"));
+
+        add(this.originalFileSize);
+
+        add(this.compressedFileSize);
+
+        add(this.compressionDegree);
+
+        add(this.compressionSpeed);
+
+        add(this.compressionTime);
     }
 }
