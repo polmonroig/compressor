90b86e3ba35e029ee47242eab8293668cbc8b725
DavidSantosss <55484949+DavidSantosss@users.noreply.github.com>
2019-12-14 16:55:00 +0100

    Add files via upload

--- FONTS/domain/GlobalStats.java
+++ FONTS/domain/GlobalStats.java
@@ -29,7 +29,6 @@ public class GlobalStats extends Stats {
     }
 
 
-
     public void addCompressionTime(float compressionTime) {
         setCompressionTime(updateValue(getCompressionTime(), compressionTime));
     }
@@ -44,5 +43,43 @@ public class GlobalStats extends Stats {
 
     public void addCompressionSpeed(float compressionSpeed) {
         setCompressionSpeed(updateValue(getCompressionSpeed(), compressionSpeed));
+    }
+
+    public String getAllStats(){
+        String data = "";
+        data += Float.toString(getNumberFiles()) + "\n";
+        data += Float.toString(getOriginalFileSize()) + "\n";
+        data += Float.toString(getCompressionTime()) + "\n";
+        data += Float.toString(getCompressedFileSize()) + "\n";
+        data += Float.toString(getCompressionDegree()) + "\n";
+        data += Float.toString(getCompressionSpeed()) + "\n";
+        return data;
+    }
+
+    public void setAllStats(String data){
+        boolean finish = false;
+        int j = 0;
+        for(int i = 0; !finish; ++i){
+            String number = "";
+            for(; data.charAt(j) != '\n'; ++j){
+                number += data.charAt(j);
+            }
+            if(i == 0)  setNumberFiles(Float.parseFloat(number));
+            else if (i == 1)    setOriginalFileSize(Float.parseFloat(number));
+            else if (i == 2)    setCompressionTime(Float.parseFloat(number));
+            else if (i == 3)    setCompressedFileSize(Float.parseFloat(number));
+            else if (i == 4)    setCompressionDegree(Float.parseFloat(number));
+            else if (i == 5)    setCompressionSpeed(Float.parseFloat(number));
+            else finish = true;
+        }
+    }
+
+    public void resetStats(){
+        setNumberFiles(0);
+        setCompressionSpeed(0);
+        setCompressionDegree(0);
+        setCompressedFileSize(0);
+        setCompressionTime(0);
+        setOriginalFileSize(0);
     }
 }
