6e3a30377aed7387642dd9625c5ee07a92d33ab9
Adry-byte <adrianalvarez0000@gmail.com>
2019-12-20 11:04:38 +0100

    Muchas cosas

--- FONTS/domain/GlobalStats.java
+++ FONTS/domain/GlobalStats.java
@@ -1,19 +1,29 @@
 package domain;
 
 /**
- *
- * **/
+ * These is the class to store the global stats of all the aplication
+ **/
 
 
 public class GlobalStats extends Stats {
+    /**
+     * In this variable we store the number of compressions
+     **/
     private float nFiles = 0;
 
-
-    public GlobalStats(){
+    /**
+     * <p>Main creator function of the class</p>
+     **/
+    public GlobalStats() {
         reset();
     }
 
-    public void setStats(Stats stats){
+    /**
+     * <p>setStats set all the necessary stats</p>
+     *
+     * @param stats A class of stats
+     **/
+    public void setStats(Stats stats) {
         setCompressionSpeed(stats.getCompressionSpeed());
         setCompressionDegree(stats.getCompressionDegree());
         setCompressedFileSize(stats.getCompressedFileSize());
@@ -21,41 +31,87 @@ public class GlobalStats extends Stats {
         setOriginalFileSize(stats.getOriginalFileSize());
     }
 
+    /**
+     * <p>getNumberFiles get the number of files compressed</p>
+     *
+     * @return float with the number of files
+     **/
+
     public float getNumberFiles() {
         return nFiles;
     }
 
- 
+    /**
+     * <p>setNumberFiles set the number of files compressed</p>
+     *
+     * @param nFiles float with the number of files
+     **/
     public void setNumberFiles(float nFiles) {
         this.nFiles = nFiles;
     }
 
-    private float updateValue(float originalValue, float addedValue){
+    /**
+     * <p>updateValue updates the value of the stats</p>
+     *
+     * @param addedValue    float with the new value to add
+     * @param originalValue float with the old value
+     * @return float with the value updated
+     **/
+    private float updateValue(float originalValue, float addedValue) {
         return ((originalValue * (nFiles - 1)) + addedValue) / nFiles;
     }
 
+    /**
+     * <p>addOriginalSize adds the new original size file of a new compression</p>
+     *
+     * @param originalFileSize float with the original size of the file
+     **/
     public void addOriginalSize(float originalFileSize) {
         setOriginalFileSize(updateValue(getOriginalFileSize(), originalFileSize));
     }
 
-
+    /**
+     * <p>addCompressionTime adds the new compression time of a new compression</p>
+     *
+     * @param compressionTime float with the compression time of the file
+     **/
     public void addCompressionTime(float compressionTime) {
         setCompressionTime(updateValue(getCompressionTime(), compressionTime));
     }
 
+    /**
+     * <p>addCompressedSize adds the new compressed size file of a new compression</p>
+     *
+     * @param compressedFileSize float with the compressed size of the file
+     **/
     public void addCompressedSize(float compressedFileSize) {
         setCompressedFileSize(updateValue(getCompressedFileSize(), compressedFileSize));
     }
 
+    /**
+     * <p>addCompressionDegree adds the new compression degree of a new compression</p>
+     *
+     * @param compressionDegree float with the compression degree of the file
+     **/
     public void addCompressionDegree(float compressionDegree) {
         setCompressionDegree(updateValue(getCompressionDegree(), compressionDegree));
     }
 
+    /**
+     * <p>addCompressionSpeed adds the new compression speed of a new compression</p>
+     *
+     * @param compressionSpeed float with the compression speed of the file
+     **/
     public void addCompressionSpeed(float compressionSpeed) {
         setCompressionSpeed(updateValue(getCompressionSpeed(), compressionSpeed));
     }
-//escribir el formato del archivo de texto
-    public String getAllStats(){
+
+    /**
+     * <p>getAllStats makes the string to write in the file of global stats</p>
+     *
+     * @return A string with the global stats
+     **/
+    public String getAllStats() {
         String data = "";
         data += getNumberFiles() + "\n";
         data += getOriginalFileSize() + "\n";
@@ -65,25 +121,29 @@ public class GlobalStats extends Stats {
         data += getCompressionSpeed() + "\n";
         return data;
     }
-    //leer el formato del archivo de texto
-    public void setFileStats(String data){
+
+    /**
+     * <p>setAllStats reads all the stats from the data parameter</p>
+     *
+     * @param data A string with the global stats
+     **/
+    public void setFileStats(String data) {
         int j = 0;
         int nStats = 5;
-        for(int i = 0; i <= nStats; ++i){
+        for (int i = 0; i <= nStats; ++i) {
             StringBuilder number = new StringBuilder();
-            while(data.charAt(j) != '\n'){
+            while (data.charAt(j) != '\n') {
                 number.append(data.charAt(j));
                 ++j;
             }
             ++j;
-            if(i == 0)  setNumberFiles(Float.parseFloat(number.toString()));
-            else if (i == 1)    setOriginalFileSize(Float.parseFloat(number.toString()));
-            else if (i == 2)    setCompressionTime(Float.parseFloat(number.toString()));
-            else if (i == 3)    setCompressedFileSize(Float.parseFloat(number.toString()));
-            else if (i == 4)    setCompressionDegree(Float.parseFloat(number.toString()));
-            else if (i == 5)    setCompressionSpeed(Float.parseFloat(number.toString()));
+            if (i == 0) setNumberFiles(Float.parseFloat(number.toString()));
+            else if (i == 1) setOriginalFileSize(Float.parseFloat(number.toString()));
+            else if (i == 2) setCompressionTime(Float.parseFloat(number.toString()));
+            else if (i == 3) setCompressedFileSize(Float.parseFloat(number.toString()));
+            else if (i == 4) setCompressionDegree(Float.parseFloat(number.toString()));
+            else if (i == 5) setCompressionSpeed(Float.parseFloat(number.toString()));
         }
     }
-
 
 }
